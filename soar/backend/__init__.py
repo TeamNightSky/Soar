761c4461ad13462bdab591d70be31789d293c3ab
Zeb Taylor <zceboys@gmail.com>
2021-08-20 19:54:58 -0500

    Made minor changes to backend setup method and debug message

--- soar/backend/__init__.py
+++ soar/backend/__init__.py
@@ -4,6 +4,10 @@ from .chat import Chat
 
 
 def setup(app, debug):
-    Auth(app, debug)
-    Chat(app, debug)
-    Channels.setup(app)
+    auth = Auth(app, debug)
+    chat = Chat(app, debug)
+    channels = Channels(app)
+
+    modules = [auth, chat, channels]
+    for module in modules:
+        module.link([x for x in modules if x is not module])
--- soar/backend/supporters.py
+++ soar/backend/supporters.py
@@ -3,7 +3,7 @@ class BackendClass:
     def __init__(self, app, debug=False):
         self.debug = debug
         if self.debug:
-            print(f"Initiating {self.__class__.__name__} backend class")
+            print(f"Initiating {self.__class__.__name__} backend class.")
         self.app = app
         self.db = app.ctx.db.db
         self.modules = {}
@@ -12,6 +12,8 @@ class BackendClass:
         self.modules = {}
         for module in modules:
             self.modules[module.__class__.__name__] = module
+        if self.debug:
+            print(f"{self.__class__.__name__} has been linked to {len(modules)} other backend classes.")
 
 
 class Error(Exception):
